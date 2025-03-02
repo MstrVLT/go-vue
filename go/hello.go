b1ca56df3b28d53db77056563853986fb9c7a911
mstrVLT <mstrvlt@users.noreply.github.com>
2025-03-02 16:30:40 +0300

    add worst case

--- go/hello.go
+++ go/hello.go
@@ -1,8 +1,17 @@
 package main
 
+import "fmt"
+
 //go:wasmexport add
 func add(a int32, b int32) int32 {
 	return a + b
 }
 
-func main() {}
+func main() {
+	const max = 100000000
+	for i := 0; i < max; i++ {
+		if i%(max/10) == 0 {
+			fmt.Println(i)
+		}
+	}
+}
